f0d514223982bfa6deddc7fdbdece6c6969cc5b2
xxzelatron <forsubscriptions007@gmail.com>
2019-08-28 08:27:05 -0700

    modified comments

--- hello.cpp
+++ hello.cpp
@@ -5,15 +5,18 @@ int main(){
 	return 0;
 }
 
-// why iostream ? 
-// >>iostream is header from input output stream 
-// >>library. Header contains std::cout ot std::cin 
-// >>objects
+/*
+Q: why iostream ? 
+Ans:iostream is header from input output stream 
+library. Header contains std::cout ot std::cin objects
+*/
 
 
+/*
+Q. why int main ? why not char main ? 
+reuturn code from int is used by 
+standard runtime library. unix and windows 
+supports concept of int reutrn value from process. 
+*/
 
-// why int main ? why not char main ? 
-// >>reuturn code from int is used by 
-// >>standard runtime library. unix and windows 
-// >>supports concept of int reutrn value from process. 
  
