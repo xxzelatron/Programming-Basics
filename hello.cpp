0e7589e5303e42d5e579efe4d8571ff19bac1bfa
xxzelatron <forsubscriptions007@gmail.com>
2019-08-28 08:23:01 -0700

    hello to C++ world

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Hello World";
+	return 0;
+}
+
+// why iostream ? 
+// >>iostream is header from input output stream 
+// >>library. Header contains std::cout ot std::cin 
+// >>objects
+
+
+
+// why int main ? why not char main ? 
+// >>reuturn code from int is used by 
+// >>standard runtime library. unix and windows 
+// >>supports concept of int reutrn value from process. 
+ 
